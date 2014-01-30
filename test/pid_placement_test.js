35393b0ca2eae3b7448738b32f3e56ffbe3e322a
James Lal <james@lightsofapollo.com>
2014-01-30 02:08:50 -0800

    start/stop have same interface now and are easier to use

---
+++ test/pid_placement_test.js
@@ -0,0 +1,28 @@
+suite('pid placement', function() {
+  var run = require('./run');
+  var fs = require('fs');
+
+  function pid(folder) {
+    return __dirname + '/' + folder + '/.ngrok_services.pid';
+  }
+
+  teardown(function() {
+    return run(['stop']);
+  });
+
+  test('pid is created in the folder with the config', function() {
+    return run(['start', 'pid1']).then(
+      function() {
+        assert.ok(fs.existsSync(pid('pid1')), 'creates pid');
+      }
+    ).then(
+      function() {
+        return run(['stop', 'pid1']);
+      }
+    ).then(
+      function() {
+        assert.ok(!fs.existsSync(pid('pid1')), 'removes pid');
+      }
+    );
+  });
+});
